767517c7e5daeffe206d8fac2c58545bae7705d8
jonnyske7ch <jonathan.porter47@gmail.com>
2017-02-20 18:30:33 -0500

    updated frame color

--- videoRecognition.py
+++ videoRecognition.py
@@ -8,10 +8,10 @@ out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
 
 while True:
 	ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    out.write(frame)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	out.write(frame)
 	cv2.imshow('Frame', frame)
-    cv2.imshow('Gray Frame', gray)
+	cv2.imshow('Gray Frame', gray)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
