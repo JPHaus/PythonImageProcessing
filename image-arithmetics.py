993175764debd899a78d40c80690305650fbf1c4
jonnyske7ch <jporter@triumvirate.com>
2017-02-21 09:36:38 -0500

    updated file

--- image-arithmetics.py
+++ image-arithmetics.py
@@ -24,9 +24,18 @@ img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 # threshold, if above 220, convert 225, if below convert to black
 ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
 
+mask_inv = cv2.bitwise_not(mask)
+img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
+
+dst = cv2.add(img1_bg, img2_fg)
+img1[0:rows, 0:cols] = dst
+
+cv2.imshow('res', img1)
 
 #cv2.imshow('add', add)
 #cv2.imshow('weighted', weighted)
-cv2.imshow('mask', mask)
+#cv2.imshow('mask', mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
