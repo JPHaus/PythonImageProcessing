d4a7cf80f485eac60014a474e4496758c6e8efb9
jonnyske7ch <jporter@triumvirate.com>
2017-02-21 09:25:51 -0500

    updated image arithmetics

--- image-arithmetics.py
+++ image-arithmetics.py
@@ -1,2 +1,32 @@
 import numpy as np
 import cv2
+
+img1 = cv2.imread('3D-Matplotlib.png')
+#img2 = cv2.imread('mainsvmimage.png')
+img2 = cv2.imread('mainlogo.png')
+
+
+# keeps opaqueness
+#add = img1 + img2
+
+# Adds pixel values together
+# ex: (155, 211, 79) + (50, 170, 200) = 205, 381, 279... translated to (205, 255, 255), 255 is max
+#add = cv2.add(img1, img2)
+
+# weighted values add up to 1, last number = gamma value
+#weighted = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
+
+rows,cols,channels = img2.shape
+roi = img1[0:rows, 0:cols]
+
+img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# threshold, if above 220, convert 225, if below convert to black
+ret, mask = cv2.threshold(img2gray, 220, 255, cv2.THRESH_BINARY_INV)
+
+
+#cv2.imshow('add', add)
+#cv2.imshow('weighted', weighted)
+cv2.imshow('mask', mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
